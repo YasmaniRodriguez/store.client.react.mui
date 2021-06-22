d831fe7f33e84d58b2da492aaf590cbc0554a0d6
rodriguez yasmani <rodriguez.yasmani@hotmail.com>
2021-06-22 00:39:36 -0300

    implementando card de material ui

--- src/components/widgets/Item/Item.jsx
+++ src/components/widgets/Item/Item.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
-import { itemStyle } from './ItemStyles.js';
-
-const useStyles = makeStyles((theme) => itemStyle(theme));
+import { Card, CardMedia, CardContent, Typography, makeStyles} from '@material-ui/core';
 
 export const Item = ({id, category, name, description, price, icon}) => {
 
     const classes = useStyles();
 
-    return <div className={classes.container}>
-        <div className='product-galery'>
-            <img src={icon}/>
-        </div>
-        <div className='product-description'>
-            <p>{price}</p>
-            <p>{name}</p>
-        </div>
-    </div>
+    return <Card className={classes.item}>
+            <CardMedia className={classes.media}
+            image={icon}
+            style={{backgroundSize: 'contain'}}
+            />
+        <CardContent>
+            <Typography variant="h5" component="h2">{name}</Typography>
+            <Typography>{price}</Typography>
+            <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+        </CardContent>
+    </Card>
 }
+
+const useStyles = makeStyles((theme) => ({
+    item: {
+        maxWidth: '300px',
+        margin: '1em',
+        boxSizing: 'border-box'
+    },
+    media: {
+        minHeight: '200px'
+    }
+}));
