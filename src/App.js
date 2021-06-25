b4d06de3d8bd936de3859ecb72794f369fa7fea2
rodriguez yasmani <rodriguez.yasmani@hotmail.com>
2021-06-25 00:09:02 -0300

    esta mierda no funciona

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BusinessContextProvider } from './components/contexts/BusinessContext.js';
 import { NavBar } from './components/widgets/NavBar/NavBar.jsx';
 import { ItemListContainer } from './components/screens/ItemListContainer/ItemListContainer.jsx';
-import { ItemDetailContainer } from './components/screens/ItemDetailContainer/ItemDetailContainer.jsx';
+//import { ItemDetailContainer } from './components/screens/ItemDetailContainer/ItemDetailContainer.jsx';
 
 const App = props => {
 
-  return <BrowserRouter>
-  <NavBar/>
-    <Switch>
-      <Route exact path="/">
-         <ItemListContainer/>
-      </Route>
-      <Route path="/category/:onlyShowCategory">
-          <ItemListContainer/>
-      </Route>
-      <Route path="/product/:onlyShowProduct">
-          <ItemDetailContainer/>
-      </Route>
-    </Switch>
-  </BrowserRouter>
+  return <>
+    <BusinessContextProvider>
+      <BrowserRouter>
+        <NavBar/>
+        <Switch>
+          <Route exact path="/">
+            <ItemListContainer/>
+          </Route>
+        </Switch>
+    </BrowserRouter>      
+    </BusinessContextProvider>
+  </>
 }
 
 export default App;
--- src/components/widgets/ItemList/ItemList.jsx
+++ src/components/widgets/ItemList/ItemList.jsx
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react';
 import { Grid } from '@material-ui/core';
 import { Item } from '../Item/Item.jsx';
-import { BusinessContext, BusinessContextProvider } from '../../screens/ItemListContainer/ItemListContainer.jsx';
+import { BusinessContextProvider } from '../../contexts/BusinessContext';
 
 export const ItemList = props => {
 
-    const productsToShow = useContext(BusinessContext);
+    const {products} = useContext(BusinessContextProvider);
+
+    console.log(products);
     
     return <BusinessContextProvider>
         <Grid container spacing={24} justify="center">
             {
-                productsToShow.map((product, i) =>
+                products.map((product, i) =>
                 <React.Fragment key={i}>
                     <Item index={i} {...product}/>
                 </React.Fragment>)
